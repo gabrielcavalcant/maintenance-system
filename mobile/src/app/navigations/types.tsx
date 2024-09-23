9384f5d9de1d03db6ffa06a9e822fa694653a4ad
gabrielcavalcant <gabriel_sep@outlook.com>
2024-09-23 17:15:45 -0300

    refactor: enhance type definitions for navigation

--- mobile/src/app/navigations/types.tsx
+++ mobile/src/app/navigations/types.tsx
@@ -8,6 +8,7 @@ export type RootStackParamList = {
   TeamScreen: undefined; // Verifique se está aqui
   CreateMaintenanceScreen: undefined;
   Details: { name: string; email: string };
+  TeamMember: undefined;
 };
 
 // Definindo a interface para os parâmetros que a tela MaintenanceScreen recebe
